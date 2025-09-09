522be7989078735e69732fc04160ecce04e67f1a
Ben Lee <ben.lee8602@gmail.com>
2025-09-09 03:47:31 -0400

    add bash profile and xinitrc
    and replace stow with ln, no point when you have a script

--- dotman.sh
+++ dotman.sh
@@ -3,32 +3,43 @@
 dotfile_dir=$(cd $(dirname $0) && pwd)
 
 declare -A targets=(
-    [alacritty]="$HOME/.config/alacritty"
-    [i3]="$HOME/.config/i3"
-    [nvim]="$HOME/.config/nvim"
-    [picom]="$HOME/.config/picom"
-    [polybar]="$HOME/.config/polybar"
+    ["alacritty"]=".config/"
+    [".bash_profile"]=""
+    ["i3"]=".config/"
+    ["nvim"]=".config/"
+    ["picom"]=".config/"
+    ["polybar"]=".config/"
+    [".xinitrc"]=""
 )
 
 action=$1
-
-if [ "$action" = "install" ]; then
-    for package in "${!targets[@]}"; do
-        target="${targets[$package]}"
-        mkdir -p $target
-        stow -d $dotfile_dir -t $target $package
-        echo "installed package $package to $target"
-    done
-
-elif [ "$action" = "uninstall" ]; then
-    for package in "${!targets[@]}"; do
-        target="${targets[$package]}"
-        stow -D -d $dotfile_dir -t $target $package
-        rmdir $target --ignore-fail-on-non-empty
-        echo "uninstalled package $package from $target"
-    done
-
-else
-    echo "usage: $0 {install|uninstall}"
+if [[ "$action" != "install" && "$action" != "uninstall" ]]; then
+    echo "usage: $0 [install|uninstall]"
+    exit 1
 fi
+
+install() {
+    local src=$1
+    local dst=$2
+    if [[ ! -e "$dst" ]]; then
+        ln -s $1 $2
+    fi
+}
+
+uninstall() {
+    local src=$1
+    local dst=$2
+    local ptr=$(readlink -f $dst)
+    if [[ "$ptr" == "$src" ]]; then
+        unlink $dst
+    fi
+}
+
+for package in "${!targets[@]}"; do
+    target="${targets[$package]}"
+    src="${dotfile_dir}/${package}"
+    dst="${HOME}/${target}${package}"
+    echo "${action}ing package $package at $dst"
+    "$action" "$src" "$dst"
+done
 
